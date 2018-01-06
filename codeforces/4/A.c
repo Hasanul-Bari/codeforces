93283b9543ddb848ff1c43232d112c5ae222690f
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2018-01-06 19:42:00 +0600

    Add solution for problem `A - Watermelon` Link: https://codeforces.com/contest/4/problem/A Tags: brute force, math, *800 Ref: https://codeforces.com/contest/4/submission/33963786

---
+++ codeforces/4/A.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int w;
+
+    scanf("%d", &w);
+
+    if(w%2==0 && w!=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+
+
+    return 0;
+}
