140b0971d3e2a942cf63b44cf582edb501161c18
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2019-03-19 21:54:00 +0600

    Add solution for problem `A - Game 23` Link: https://codeforces.com/contest/1141/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1141/submission/51520188

---
+++ codeforces/1141/A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsignd long long
+using namespace std;
+
+int main()
+{
+    ios :: sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll src,des;
+    cin>>src>>des;
+
+
+    queue <ll> q;
+
+    map<ll,ll> visited;
+    map<ll,ll> level;
+    //map<int,int> :: iterator itr;
+
+
+    visited[src]=1;
+
+    q.push(src);
+
+    while(q.empty()!=1)
+    {
+        ll x=q.front();
+        q.pop();
+
+        for(ll i=2; i<=3; i++)
+        {
+            ll y=x*i;
+            if(visited[y]==0 && y<=des)
+            {
+                visited[y]=1;
+                level[y]=level[x]+1;
+                q.push(y);
+            }
+        }
+    }
+    if(src==des)
+        cout<<0<<endl;
+    else if(level[des]==0)
+        cout<<-1<<endl;
+    else
+
+        cout<<level[des]<<endl;
+
+
+    return 0;
+}
