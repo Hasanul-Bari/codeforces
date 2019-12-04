638aec9baff8869874208883de0a73b520bd88d8
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2019-12-05 00:36:00 +0600

    Add solution for problem `D - Cubical Planet` Link: https://codeforces.com/contest/39/problem/D Tags: math, *1100 Ref: https://codeforces.com/contest/39/submission/66284432

---
+++ codeforces/39/D.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    bool x,y,z,a,b,c,ans=true;
+
+    cin>>x>>y>>z>>a>>b>>c;
+
+
+    if(x!=a && y!=b && z!=c)
+        cout<<"NO"<<endl;
+    else
+        cout<<"YES"<<endl;
+
+
+    return 0;
+}
