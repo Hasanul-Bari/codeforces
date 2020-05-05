2191749c19ea544e14fa64cc2f256eac923c60be
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-05-05 16:00:00 +0600

    Add solution for problem `D - Queue` Link: https://codeforces.com/contest/545/problem/D Tags: greedy, implementation, sortings, *1300 Ref: https://codeforces.com/contest/545/submission/79044276

---
+++ codeforces/545/D.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    int n;
+    cin>>n;
+
+    int a[n];
+
+    for(int i=0; i<n; i++)
+        cin>>a[i];
+
+    sort(a,a+n);
+
+    ll s=0,c=0;
+    for(int i=0; i<n; i++)
+    {
+        if(s<=a[i])
+        {
+            c++;
+
+            s=s+a[i];
+        }
+
+    }
+
+    cout<<c<<endl;
+
+
+
+
+    return 0;
+}
