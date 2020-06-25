d71188caed166b625441b3c3943e5b216d5ff600
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-06-25 14:38:00 +0600

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84952510

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    int t,n;
+    cin>>t;
+
+    while(t--)
+    {
+
+        cin>>n;
+        if(n%4==0)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+
+
+
+
+    return 0;
+}
