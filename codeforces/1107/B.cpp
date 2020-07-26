8fc52fc81687f22d246c75029bf816966eae3eab
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-07-26 15:03:00 +0600

    Add solution for problem `B - Digital root` Link: https://codeforces.com/contest/1107/problem/B Tags: math, number theory, *1000 Ref: https://codeforces.com/contest/1107/submission/88042058

---
+++ codeforces/1107/B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    ll t,k,x;
+
+    cin>>t;
+
+    while(t--)
+    {
+        cin>>k>>x;
+
+        cout<<((k-1)*9)+x<<endl;
+    }
+
+
+
+    return 0;
+}
