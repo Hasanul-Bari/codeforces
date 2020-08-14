d52bb5b447029e406c197a59e9752d3ef27f7f08
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-08-14 19:49:00 +0600

    Add solution for problem `A - Raising Bacteria` Link: https://codeforces.com/contest/579/problem/A Tags: bitmasks, *1000 Ref: https://codeforces.com/contest/579/submission/89875516

---
+++ codeforces/579/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    int x;
+    cin>>x;
+
+    cout<<__builtin_popcount(x)<<endl;
+
+
+
+
+    return 0;
+}
