a93ddf4bd9bb4a20fe0e990080956af7e803042e
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-12-25 21:11:00 +0600

    Add solution for problem `A - Strange Functions` Link: https://codeforces.com/contest/1455/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1455/submission/102329508

---
+++ codeforces/1455/A.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    int t;
+    cin>>t;
+
+    string s;
+
+    while(t--)
+    {
+        cin>>s;
+        cout<<s.length()<<endl;
+    }
+
+
+
+
+    return 0;
+}
